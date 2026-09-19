5beaf6ae82ebf6087b413a7baa7e9514465486df
seoyoon lee <leeseoyoon01@gmail.com>
2026-09-19 20:18:49 +0900

    feat: footer 구현

--- leeseo/week2/umcine/src/components/header.tsx
+++ leeseo/week2/umcine/src/components/header.tsx
@@ -1,6 +1,7 @@
 import "../App.css";
 import "../style/buttons.css"
 import "../style/texts.css"
+import "../style/images.css"
 import movie from "../../public/icons/movie.svg"
 import { IoMdSearch } from "react-icons/io";
 
@@ -9,8 +10,10 @@ const Header = () => {
         <>
             <div className="headerGrid">
                 <div id="headerLeft">
-                    <img src={movie}/>
-                    <div id="logoText">UMCine</div>
+                    <div id="logoGrid">
+                        <img id="logoImage" src={movie}/>
+                        <div id="logoText">UMCine</div>
+                    </div>
                     <button className="textButton">영화</button>
                     <button className="textButton">검색</button>
                     <button className="textButton">내 정보</button>
