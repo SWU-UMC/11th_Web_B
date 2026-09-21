37cb5e78be83354016ac6765dace08610df9ddcd
seoyoon lee <leeseoyoon01@gmail.com>
2026-09-21 21:48:01 +0900

    feat: 영화목록 텍스트 추가

--- leeseo/week2/umcine/src/components/movie-grid.tsx
+++ leeseo/week2/umcine/src/components/movie-grid.tsx
@@ -1,14 +1,21 @@
 import MovieCard from "./movie-card";
 import { movies } from "../types/movies";
+import "../App.css";
+import "../style/texts.css"
 
 const MovieGrid = () => {
 
     return (
         <>
-            <div id="movieGrid">
-                {movies.map((movie) => (
-                    <MovieCard movie={movie}/>
-                ))}
+            <div className="container">
+                <div id="movieListTitleGrid">
+                <div id="movieListTitleText">영화 목록</div>
+                </div>
+                <div id="movieGrid">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
             </div>
         </>
     )
